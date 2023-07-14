64316c038bd8816dd197734418063119708099a3
matthewsm <matthew.smith@derivco.co.uk>
2023-07-14 14:13:33 +0100

    istqbtest.feature: add test steps to feature file cypress.config.js: add folder locations

--- cypress.config.js
+++ cypress.config.js
@@ -2,6 +2,9 @@ module.exports = {
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
+      
     },
+    integrationFolder: 'features',
+    stepDefinitions: 'steps',
   },
 };
